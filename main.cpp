6241c46cfab48ff70f13d9148be932196f9b39cc
Iago Abner <iagoabner.dev@gmail.com>
2022-12-27 21:38:12 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,71 +1,104 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "DHT.h"
 
-const int SAMPLES= 10;
+#define DHT11PIN 16
 
-#define CLIENT "ESP RFID"        
-#define TYPE "ESP32"              
+DHT dht(DHT11PIN, DHT11);
 
-RTC_DATA_ATTR char name[15] = CLIENT;
+const char* ssid = "00";
+const char* password = "mblt1234";
 
-StaticJsonDocument<500> doc;
+const char* serverName = "https://next-temp-mu.vercel.app/api";
 
-const int LM35 = 13; 
-float reading; 
+const char* root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----";
 
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-const char* serverName = "http://localhost:3000/users";
-
+char jsonOutput[128];
+  
 void setup() {
-  Serial.begin(9600);
-  Serial.print("Connecting to: ");
-  Serial.print(ssid);
-  Serial.print(" with password: ");
-  Serial.println(password);
+  
+  Serial.begin(115200);
+  delay(4000);  
 
-  WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  dht.begin();
+  
+  WiFi.begin(ssid, password); 
+  
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
+  
+  Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
+  
 }
-
+  
 void loop() {
-  sensorTemp();
-  delay(10000);
-  Serial.println("Posting...");
-  postData();
-  serializeJsonPretty(doc, Serial);
-  Serial.println("Done.");
-}
 
-float sensorTemp() {
-  reading = (float(analogRead(LM35))*5/(1023))/0.01;
+ float humi = dht.readHumidity();
+ float temp = dht.readTemperature();
+ Serial.println(humi);
+ Serial.println(temp);
+ if(WiFi.status()== WL_CONNECTED){   
+   HTTPClient http;
+   
+   http.begin(serverName, root_ca); 
+   
+   http.addHeader("Content-Type", "application/json");
+   String httpRequestData = "{\"temperature\":\"" + String(temp) + "\",\"humidity\":\"" + String(humi) + "\"}";
+   int httpCode = http.POST(httpRequestData);
 
-  doc["temperature"] = reading;
+   if (httpCode > 0) {
+       String payload = http.getString();
+       Serial.println(httpCode);
+       Serial.println(payload);
+    }
 
-  return reading;
-}
+    else {
+      Serial.println("Error on HTTP request");
+    }
 
-void postData() {     
-      if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
-
-      http.begin(serverName);
-      http.addHeader("Content-Type", "application/json");
-
-      String json;
-      serializeJson(doc["temperature"], json);
-      Serial.println("Dados armazenados para o POST");
-      Serial.println(json);
-      int httpResponseCode = http.POST(json);
-      Serial.println("POSTED");
-      Serial.println(httpResponseCode);
-      }
+   http.end();
+  
+ }else{
+  
+    Serial.println("Error in WiFi connection");   
+  
+ }
+  
+  delay(5000); 
+  
 }
